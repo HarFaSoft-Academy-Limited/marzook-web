f703e020626a339e2d20a3de4b77025915181d72
user <user@Najibs-MacBook-Pro.local>
2025-05-27 22:48:59 +0100

    static files

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,26 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+//   basePath: "/marzook-web",
+//   output: "export",
+//   reactStrictMode: true,
+// }
+
+// export default nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
   basePath: "/marzook-web",
   output: "export",
   reactStrictMode: true,
-}
+};
 
-export default nextConfig
+module.exports = nextConfig;
