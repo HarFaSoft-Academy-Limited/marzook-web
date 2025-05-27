575aa9986ee8b6e1e90b0df8776234714a97eb41
user <user@Najibs-MacBook-Pro.local>
2025-05-27 22:52:41 +0100

    nex config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/marzook-web",
+    output: "export",
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
