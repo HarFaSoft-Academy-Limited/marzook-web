5e8d89e356def1b69e3704aebddf3940f7959417
user <user@Najibs-MacBook-Pro.local>
2025-07-23 20:11:27 +0100

    linting

--- app/dashboard/admin/students/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import StudentProfile from "./student-profile";
-
-export async function generateStaticParams() {
-  return [];
-}
-
-export default function StudentProfilePage() {
-  return <StudentProfile />;
-}
